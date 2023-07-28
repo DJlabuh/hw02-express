74606f54f36651b509b1af57ad12b12bfd62f926
Oleksii Leschenko <djlabuh@gmail.com>
2023-07-28 19:58:13 +0300

    add login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -14,4 +14,10 @@ authRouter.post(
   authController.signup
 );
 
+authRouter.post(
+  "/signin",
+  validateBody(userSchemas.userSigninSchema),
+  authController.signin
+);
+
 export default authRouter;
